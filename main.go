ae0a9e4e0821e5ebfb3e4c263e9b11f457f97c5c
yangkai <yangkai@p1.com>
2019-09-04 18:44:04 +0800

    add example go files

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello gopher!")
+}
+
+// Calculate returns x + 2.
+func Calculate(x int) (result int) {
+	result = x + 2
+	return result
+}
