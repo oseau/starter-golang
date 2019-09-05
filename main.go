8c693f0f68c7d1e808ad519fa30c896a77097741
yangkai <yangkai@p1.com>
2019-09-05 18:18:22 +0800

    exclude main by adding test tag

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
+//+build !test
+
 package main
 
 import "fmt"
 
 func main() {
 	fmt.Println("hello gopher!")
-}
-
-// Calculate returns x + 2.
-func Calculate(x int) (result int) {
-	result = x + 2
-	return result
 }
